d69418a89f183ee9b6c71b4174c50d7955ff697b
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-30 05:16:43 +0000

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,23 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      title TEXT,
+      length INTEGER
+      )
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end
